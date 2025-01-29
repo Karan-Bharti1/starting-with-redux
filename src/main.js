c9ec1514f8a87871d7456e729afe1a76fe6cdbea
Karan-Bharti1 <karan.codes1@gmail.com>
2025-01-29 08:51:50 +0530

    feat: provided eventlisteners to buttons and updated cookie count on view

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import './style.css'
 import { createStore } from 'redux'
 import cookieReducer from './cookieReducer'
-
+const addCookie=document.getElementById("addCookie")
+const removeCookie=document.getElementById("removeCookie")
+const cookieCount=document.getElementById("cookieCount")
 const store=createStore(cookieReducer)
-store.subscribe(()=>console.log(store.getState()))
+store.subscribe(()=>updatedCookie())
 
-// store.dispatch({type:"cookies/added"})
-// store.dispatch({type:"cookies/added"})
-// store.dispatch({type:"cookies/removed"})
+const addCookieHandler=()=>{
+    store.dispatch({type:"cookies/added"})
+}
+const removeCookieHandler=()=>{
+    store.dispatch({type:"cookies/removed"})
+}
+
+addCookie.addEventListener("click",addCookieHandler)
+addCookie.addEventListener("click",addCookieHandler)
+removeCookie.addEventListener("click",removeCookieHandler)
+const updatedCookie=()=>{
+    const state=store.getState()
+    cookieCount.textContent=state.value
+}
+updatedCookie()
